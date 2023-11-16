f9c2718d310100741039c9abe11b284a12ac3c5c
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2023-11-16 16:40:42 -0600

    Add PantallaPrincipal screen to app

---
+++ lib/presentation/screens/pantallaPrincipal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PantallaPrincipal extends StatelessWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: Text("Hola mundo Mau")),
+      );
+  }
+}
