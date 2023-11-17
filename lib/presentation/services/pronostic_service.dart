22cdb17be37e49b9e4918ad03ee01671cf209830
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2023-11-17 15:25:30 -0600

    Add PronosticService to fetch hourly forecast

---
+++ lib/presentation/services/pronostic_service.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<HourlyForecast>> fetchHourlyForecast(String city) async {
+  final response = await http.get(
+    Uri.parse('http://api.openweathermap.org/data/2.5/forecast?q=$city&appid=ab257ee0c6fdd62eb623c952c014d722'),
+  );
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> data = jsonDecode(response.body);
+    List<dynamic> hourlyData = data['list'];
+
+    return hourlyData.take(2).map((json) => HourlyForecast.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load hourly forecast');
+  }
+}
+
+class HourlyForecast {
+  final DateTime time;
+  final String description;
+  final double temperature;
+
+  HourlyForecast({
+    required this.time,
+    required this.description,
+    required this.temperature,
+  });
+
+  factory HourlyForecast.fromJson(Map<String, dynamic> json) {
+    return HourlyForecast(
+      time: DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000),
+      description: json['weather'][0]['description'],
+      temperature: json['main']['temp'],
+    );
+  }
+}
